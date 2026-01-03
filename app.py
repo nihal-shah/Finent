cd7bba28c09131f16edb1a8731448f531ec6db9a
nihal-shah <nihalshah10283@gmail.com>
2026-01-03 23:30:07 +0530

    integrated real api for fetching stock data

--- app.py
+++ app.py
@@ -5,8 +5,26 @@ from dotenv import load_dotenv
 
 load_dotenv()
 domain = os.environ.get("DOMAIN")
-option = st.selectbox('Select an Index Type?',('Equity', 'Fixed Income', 'Multi Asset'))
+import streamlit as st
+import datetime
 
-st.write('You selected:', option)
-response = httpx.post(domain + "/gethistoricaltypeSubindexdata", json={"indextype": option},timeout=10.0)
-st.json(response.json())
+stock_name = st.text_input("Enter your stock symbol")
+start_datetime = st.datetime_input(
+    "Select start date and time",
+    value=None
+)
+end_datetime = st.datetime_input(
+    "Select end date and time",
+    value=None
+)
+def fetch_data():
+    response = httpx.post(domain + "/gethistoricaltypeSubindexdata", json={"stock": stock_name, "start_date": start_datetime.strftime("%Y-%m-%d %H:%M:%S"), "end_date": end_datetime.strftime("%Y-%m-%d %H:%M:%S")},timeout=30.0)
+    return response
+
+st.write("You selected:", start_datetime, "to", end_datetime)
+if st.button("fetch data"):
+    # Code inside this 'if' block only executes when the button is pressed
+    response = fetch_data()
+    # st.write(response)
+    data = response.json()
+    st.json(data)
--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 from fastapi import FastAPI
 import httpx as httpx
 from model import Fetching
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+tewlvedata_api_key=os.getenv("twelvedata_api_key")
 app = FastAPI()
 
 @app.post("/gethistoricaltypeSubindexdata")
 def get_historical_type_subindex_data(fetching: Fetching):
-    indextype = fetching.indextype
-    # Dummy response for demonstration purposes
-    data = {
-        "Equity": {"data": [1, 2, 3]},
-        "Fixed Income": {"data": [4, 5, 6]},
-        "Multi Asset": {"data": [7, 8, 9]}
-    }
-    return data.get(indextype, {"data": []})
-
-
-    
+        stock=fetching.stock
+        # start=input("Enter the start date (2006-01-02 22:52:00): ")
+        start=fetching.start_date
+        end=fetching.end_date
+        response = httpx.get(f"https://api.twelvedata.com/time_series?apikey={tewlvedata_api_key}&symbol={stock}&interval=1day&start_date={start}&end_date={end}&format=JSON")
+        print(response.json())
+        return response.json()
